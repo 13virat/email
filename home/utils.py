0fe412750c12a4188688efb5d50be513c5f0a032
13virat <viratgupta1314@gmail.com>
2024-07-14 21:50:04 +0530

    with folder

--- home/utils.py
+++ home/utils.py
@@ -1,5 +1,14 @@
+import os
+import zipfile
 from django.core.mail import EmailMessage
 from django.conf import settings
+
+def zip_folder(folder_path, output_path):
+    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, os.path.relpath(file_path, folder_path))
 
 def send_email_with_attachment(subject, message, recipient_list, file_path):
     mail = EmailMessage(subject=subject, body=message, from_email=settings.EMAIL_HOST_USER, to=recipient_list)
--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
-from .utils import send_email_with_attachment
+from .utils import send_email_with_attachment, zip_folder
 
 def send_email_to_client():
     subject = "This email is from Django Server"
@@ -16,9 +16,16 @@ def send_email(request):
     subject = "This email is from Django Server hiii"
     message = "This email is a test message from the server, changed the message"
     recipient_list = ["imailvirat@gmail.com"]
-    file_path = f"{settings.BASE_DIR}/hi.xlsx"
-    send_email_with_attachment(subject, message, recipient_list, file_path)
-    return HttpResponse("Email sent!")
+    folder_path = f"{settings.BASE_DIR}/seleniumpro"
+    zip_path = f"{settings.BASE_DIR}/my_folder.zip"
+    
+    # Zip the folder
+    zip_folder(folder_path, zip_path)
+    
+    # Send the email with the zipped folder
+    send_email_with_attachment(subject, message, recipient_list, zip_path)
+    
+    return HttpResponse("Email sent with folder!")
 
 def home_view(request):
     return render(request, "home/index.html")
