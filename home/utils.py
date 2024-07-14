57d9c0b7e5beea087ad928d6608cd6e70e32c703
13virat <viratgupta1314@gmail.com>
2024-07-14 21:43:50 +0530

    sending with file attachment

--- home/utils.py
+++ home/utils.py
@@ -1,11 +1,7 @@
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.conf import settings
 
-
-def send_email_to_client():
-    subject = "This email is from Django Server"
-    message = "This email is test message server"
-    from_email = settings.EMAIL_HOST_USER
-    recipient_list = ["imailvirat@gmail.com"]
-
-    send_mail(subject, message, from_email, recipient_list)
+def send_email_with_attachment(subject, message, recipient_list, file_path):
+    mail = EmailMessage(subject=subject, body=message, from_email=settings.EMAIL_HOST_USER, to=recipient_list)
+    mail.attach_file(file_path)
+    mail.send()
--- home/views.py
+++ home/views.py
@@ -1,30 +1,24 @@
-# from django.shortcuts import redirect, render
-# from django.http import HttpResponse
-# from .utils import send_email_to_client
-# # Create your views here.
-# def send_email(request):
-#     send_email_to_client()
-#     return HttpResponse("Email sent!")
-# def home_view(request):
-#     return render(request, 'home/index.html')
-# home/views.py
-
 from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
+from .utils import send_email_with_attachment
 
 def send_email_to_client():
-    subject = "This email is from Django Server hiii "
-    message = "This email is test message server changed the message "
+    subject = "This email is from Django Server"
+    message = "This email is a test message from the server"
     from_email = settings.EMAIL_HOST_USER
     recipient_list = ["imailvirat@gmail.com"]
 
     send_mail(subject, message, from_email, recipient_list)
 
 def send_email(request):
-    send_email_to_client()
+    subject = "This email is from Django Server hiii"
+    message = "This email is a test message from the server, changed the message"
+    recipient_list = ["imailvirat@gmail.com"]
+    file_path = f"{settings.BASE_DIR}/hi.xlsx"
+    send_email_with_attachment(subject, message, recipient_list, file_path)
     return HttpResponse("Email sent!")
 
 def home_view(request):
-    return render(request, 'home/index.html')
+    return render(request, "home/index.html")
